d174c172fa927c22b5b3a94783e4ffe56cb84eda
coldDelight <hno05039@naver.com>
2024-01-12 14:28:25 +0900

    로그인 버튼 UI

--- feature/login/src/main/java/com/colddelight/login/LoginScreen.kt
+++ feature/login/src/main/java/com/colddelight/login/LoginScreen.kt
@@ -1,15 +1,11 @@
 package com.colddelight.login
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
